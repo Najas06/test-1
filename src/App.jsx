d77ab7934ea382cccf2f56b3bd3da5017401a143
Najas06 <najasneju@gmail.com>
2024-10-30 22:22:27 +0530

    setup for machine-test

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Hero from "./section/Hero"
+
+
+function App() {
+
+  return (
+   <>
+  <main className="max-w-[1440px] mx-auto">
+    <Hero/>
+  </main>
+   </>
+  )
+}
+
+export default App
---
+++ src/section/Hero.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Hero = () => {
+  return (
+    <div className='grid grid-cols-6 min-h-screen'>
+        <div className='col-span-4 h-full w-full border border-red-500'>
+        
+        </div>
+    </div>
+  )
+}
+
+export default Hero
